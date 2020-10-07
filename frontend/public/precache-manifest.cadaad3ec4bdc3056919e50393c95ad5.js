f1846f2bf9b9cd0446e1010247adf41bc198a8fe
scott <scott@goldilocks.design>
2020-10-07 04:14:02 +0100

    fork unicorn-busy-server, repair static api and unicorn methods

---
+++ frontend/public/precache-manifest.cadaad3ec4bdc3056919e50393c95ad5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "471fdae787086cc42d67de36bbf441aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4abffb9d3242ed432bbe",
+    "url": "/static/css/main.634f4f2b.chunk.css"
+  },
+  {
+    "revision": "0240123fd1163ed646b9",
+    "url": "/static/js/2.23a25711.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.23a25711.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4abffb9d3242ed432bbe",
+    "url": "/static/js/main.bd7b37b6.chunk.js"
+  },
+  {
+    "revision": "10e18084de1363b5c050",
+    "url": "/static/js/runtime-main.7cae99bc.js"
+  }
+]);
